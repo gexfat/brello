9ee1e4fb2cfaddb4f2cf0693a0600c1a268a675c
Gabe <16979062+gexfat@users.noreply.github.com>
2021-11-05 12:31:01 -0500

    styling

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about p-4">
-    <h1 class="text-shadow-light text-gray-500">Version 0.0.1.</h1>
+    <h1 class="text-shadow-light text-gray-500 leading-normal mb-2">Version 0.0.1.</h1>
+    <hr class="my-2 border-t border-gray-300 border-opacity-50" style="border-bottom: 1px solid #fff;">
+    <p class="font-bold mb-3">
+      Road Map
+    </p>
+    <ul class="inline-flex flex-col space-y-2 text-base p-2 bg-gray-200 inner-shadow rounded border border-gray-300 border-opacity-50">
+      <li>
+        Custom labels
+      </li>
+      <li>
+        Unsplash backrounds
+      </li>
+      <li>
+        Comments on tasks
+      </li>
+    </ul>
   </div>
 </template>
